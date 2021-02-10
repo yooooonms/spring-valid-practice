7bb4c728cae4d9240edf5edd2759c7fc254e98fa
yooooonms <yoonminsoo0617@gmail.com>
2021-02-10 21:10:34 +0900

    [update] 실행 시 회원 하나 저장
    이메일 중복 검사 기능 구현 예정

--- src/main/java/com/valid/practice/PracticeApplication.java
+++ src/main/java/com/valid/practice/PracticeApplication.java
@@ -1,13 +1,30 @@
 package com.valid.practice;
 
+import com.valid.practice.member.Member;
+import com.valid.practice.member.MemberRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PracticeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PracticeApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(MemberRepository memberRepository) {
+        return args -> {
+            Member newMember = Member.builder()
+                    .identifier("freems0617")
+                    .password("0000")
+                    .email("test@test.com")
+                    .build();
+
+            memberRepository.save(newMember);
+        };
     }
 
 }
